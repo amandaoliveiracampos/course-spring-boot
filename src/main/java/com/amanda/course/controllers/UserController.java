f4977c34dcba366ef8df9fa937469f9f91168003
Amanda Campos <amandaolivcampos@gmail.com>
2022-12-02 17:23:25 -0300

    User entity and resource

---
+++ src/main/java/com/amanda/course/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.amanda.course.controllers;
+
+import com.amanda.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(user);
+
+    }
+}
