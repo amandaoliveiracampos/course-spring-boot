4bb2bfad5c05dcc0b5124c23e7fb950450959ad1
Amanda Campos <amandaolivcampos@gmail.com>
2022-12-05 14:25:04 -0300

    H2 database, test profile, JPA

--- src/main/java/com/amanda/course/controllers/UserController.java
+++ src/main/java/com/amanda/course/controllers/UserController.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @GetMapping
     public ResponseEntity<User> findAll() {
         User user = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
         return ResponseEntity.ok().body(user);
-
     }
+
 }
--- src/main/java/com/amanda/course/entities/User.java
+++ src/main/java/com/amanda/course/entities/User.java
@@ -1,10 +1,16 @@
 package com.amanda.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -12,7 +18,7 @@ public class User implements Serializable {
     private String password;
 
 
-    public User(){
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
