d7fe572d124501ba538d9a4d46c895436d2b3c32
Amanda Campos <amandaolivcampos@gmail.com>
2022-12-09 17:07:05 -0300

    Exception handling - update

--- src/main/java/com/amanda/course/services/UserService.java
+++ src/main/java/com/amanda/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.amanda.course.entities.User;
 import com.amanda.course.repositories.UserRepository;
 import com.amanda.course.services.exceptions.DatabaseException;
 import com.amanda.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
